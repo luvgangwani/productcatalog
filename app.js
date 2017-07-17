a482d58d566f1c823ee6ce465208413591dc092c
Luv Gangwani <luv.gangwani@gmail.com>
2017-07-17 19:25:56 +0530

    Add products/View products

--- app.js
+++ app.js
@@ -4,12 +4,15 @@ var path = require('path');
 
 var bodyParser = require('body-parser');
 
+var mysql = require('mysql');
+
 var app = express();
 
 app.use('/scripts', express.static(__dirname + "/node_modules/bootstrap/dist"));
 app.use('/local', express.static(__dirname + "/assets"));
 app.use('/js', express.static(__dirname + "/node_modules/jquery/dist"));
-app.use(bodyParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.set('view engine', 'jade');
 
@@ -20,8 +23,79 @@ app.get('/', function(request, response){
 
 app.get('/viewproducts', function(request,response){
 
-	response.send("Page contains a list of products");
+	var connection = mysql.createConnection({
+		host: 'localhost',
+		user: 'root',
+		password: 'password_123',
+		database: 'productcatalog'
+	});
 
+	connection.connect(function(err){
+
+		if(err){
+
+			console.log("Unable to connect: " + err);
+		}
+		else {
+
+			console.log("Connected successfully!");
+
+			var sql = "SELECT pid, pname, pdesc";
+			sql += " FROM productlist";
+
+			connection.query(sql, function(err, rows, fields){
+
+				if(err){
+
+					console.log("Unable to get product data: " + err);
+				}
+				else {
+
+					// console.log(rows);
+					response.render('viewproducts', {'products':rows})
+				}
+			});
+		}
+	})
+
+});
+
+app.get('/viewproduct/:id', function(request, response){
+
+	var connection = mysql.createConnection({
+		host: 'localhost',
+		user: 'root',
+		password: 'password_123',
+		database: 'productcatalog'
+	});
+
+	connection.connect(function(err){
+
+		if(err){
+
+			console.log("Unable to connect: " + err);
+		}
+		else{
+
+			console.log("Connected successfully!");
+
+			var sql = "SELECT pname, pdesc, pimg"
+			sql += " FROM productlist"
+			sql += " WHERE pid = " + request.params['id']
+
+			connection.query(sql, function(err, rows, fields){
+
+				if(err){
+
+					console.log("Unable to get the data: " + err);
+				}
+				else{
+
+					response.render('productdetails',{'product':rows})
+				}
+			});
+		}
+	})
 });
 
 app.get('/addnewproduct', function(request, response){
@@ -32,7 +106,40 @@ app.get('/addnewproduct', function(request, response){
 
 app.post('/addnewproduct', function(request, response){
 
-	console.log(request.body);
+	var connection = mysql.createConnection({
+		host: 'localhost',
+		user: 'root',
+		password: 'password_123',
+		database: 'productcatalog'
+	});
+
+	connection.connect(function(err){
+
+		if(err){
+			console.log("Unable to connect:" + err);
+		}
+		else{
+
+			console.log("Connected successfully!");
+
+			var sql = "INSERT INTO productlist(pname, pdesc, created, updated)";
+			sql += "VALUES('"+request.body.pname+"','"+request.body.pdesc+"',"+Date.now()+","+Date.now()+");";
+
+			//console.log(sql);
+
+			connection.query(sql, function(err, result){
+
+				if(err){
+
+					console.log("Unable to add a product: "+err);
+				}
+				else{
+
+					response.send(result);
+				}
+			});
+		}
+	});
 
 });
 app.listen('3333', function(){
--- assets/js/productcatalog.js
+++ assets/js/productcatalog.js
@@ -9,17 +9,32 @@ $(document).ready(function(){
 
             'pname': $('#pname').val(),
             'pdesc': $('#pdesc').val(),
-            'pimg': $('#pimg')[0].files[0].name
+            //'pimg': $('#pimg')[0].files[0].name
         };
-        
+
         $.ajax({
 
             url: '/addnewproduct',
             method: 'post',
             data: data,
             dataType: 'json',
-            success: function(){
-                alert("Added new product");
+            success: function(data){
+                if(data != 0){
+
+                    $("form#addnewproduct .alert-success").show();
+                    setTimeout(function(){
+                        $("form#addnewproduct .alert-success").slideUp();
+                    }, 5000);
+
+                    $("form#addnewproduct")[0].reset();
+                }
+                else{
+
+                    $("form#addnewproduct .alert-warning").show();
+                    setTimeout(function(){
+                        $("form#addnewproduct .alert-warning").slideUp();
+                    }, 2000);
+                }
             }
 
         });
